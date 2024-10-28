aa0336c636f799e5f21823ba5416266892e9c113
NicolasMunozC <nmunozc94@gmail.com>
2024-10-28 14:35:11 -0300

    feat: add sonner

--- componets/UIProvider.tsx
+++ componets/UIProvider.tsx
@@ -1,12 +1,14 @@
 import * as React from "react";
 
 import {NextUIProvider} from "@nextui-org/react";
+import { Toaster } from "sonner";
 
 export default function UIProvider({children}: {
     children: React.ReactNode
 }) {
   return (
     <NextUIProvider>
+      <Toaster />
         {children}
     </NextUIProvider>
   );
