b135adf0e4964793408575e401cb2fb5b9d8e792
NicolasMunozC <nmunozc94@gmail.com>
2024-10-28 14:40:06 -0300

    feat: add next themes

--- componets/UIProvider.tsx
+++ componets/UIProvider.tsx
@@ -1,7 +1,11 @@
+'use client'
+
 import * as React from "react";
 
 import {NextUIProvider} from "@nextui-org/react";
 import { Toaster } from "sonner";
+import {ThemeProvider as NextThemesProvider} from "next-themes";
+
 
 export default function UIProvider({children}: {
     children: React.ReactNode
@@ -9,7 +13,9 @@ export default function UIProvider({children}: {
   return (
     <NextUIProvider>
       <Toaster />
+      <NextThemesProvider attribute="class" defaultTheme="light">
         {children}
+      </NextThemesProvider>
     </NextUIProvider>
   );
 }
