65de23ed6a87de327f894bc6424fd7013e446d39
NicolasMunozC <nmunozc94@gmail.com>
2024-10-29 18:25:47 -0300

    feat: add emoji favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import UIProvider from "@/componets/UIProvider";
-
 
 export const metadata: Metadata = {
   title: "Zen: Pomodoro",
@@ -13,13 +11,21 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const faviconEmoji = "🍵"; // Cambia este emoji según tus necesidades
+
   return (
     <html lang="en">
-        <body className="w-screen min-h-screen h-screen flex flex-col overflow-hidden">
-          <UIProvider>
-            {children}
-          </UIProvider>
-        </body>
+      <head>
+        <title>{"Zen: Pomodoro"}</title>
+        <meta name="description" content={"Pomodoro App, simple."} />
+        <link
+          rel="icon"
+          href={`data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 32 32%22><text y=%22.9em%22 font-size=%2224%22>${faviconEmoji}</text></svg>`}
+        />
+      </head>
+      <body className="w-screen min-h-screen h-screen flex flex-col overflow-hidden">
+        {children}
+      </body>
     </html>
   );
 }
