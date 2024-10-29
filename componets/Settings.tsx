84124d16886cfcffe84a0ae89503c01daa9bca84
NicolasMunozC <nmunozc94@gmail.com>
2024-10-29 12:32:54 -0300

    feat: add update state prop

--- componets/Settings.tsx
+++ componets/Settings.tsx
@@ -1,9 +1,12 @@
+import { Pomodoro } from "@/types/types";
 import { theme } from "@/utils/theme";
 import { Button } from "@nextui-org/react";
+import { Dispatch, SetStateAction } from "react";
 import { IoMdSettings } from "react-icons/io";
 
-export default function Settings({currentTheme}:{
-    currentTheme: "red" | "green" | "blue"
+export default function Settings({currentTheme, setPomodoro}:{
+    currentTheme: "red" | "green" | "blue",
+    setPomodoro: Dispatch<SetStateAction<Pomodoro>>
 }) {
     return (
         <Button isIconOnly className={`${theme.light.button.bg[currentTheme]} w-16 h-16 text-xl ${theme.light.text[currentTheme]} rounded-2xl`}><IoMdSettings/></Button>
