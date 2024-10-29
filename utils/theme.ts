f81ff533efd7c2e6d3e27f3e5113d72825f5dd9b
NicolasMunozC <nmunozc94@gmail.com>
2024-10-29 11:21:49 -0300

    refactor: move theme

---
+++ utils/theme.ts
@@ -0,0 +1,50 @@
+export const theme = {
+    light:{
+      bg: {
+        red: 'bg-red-50',
+        blue: 'bg-blue-50',
+        green: 'bg-green-50'
+      },
+      text: {
+        red: 'text-red-950',
+        blue: 'text-blue-950',
+        green: 'text-green-950'
+      },
+      chip:{
+        bg:{
+          red: 'bg-redAlpha-100/[0.15]',
+          blue: 'bg-blueAlpha-100/[0.15]',
+          green: 'bg-greenAlpha-100/[0.15]'
+        },
+        border:{
+          red: 'border-red-900',
+          blue: 'border-blue-900',
+          green: 'border-green-900'
+        }
+      },
+      button:{
+        bg: {
+          red: 'bg-redAlpha-100/[0.15]',
+          blue: 'bg-blueAlpha-100/[0.15]',
+          green: 'bg-greenAlpha-100/[0.15]'
+        },
+        specialBg: {
+          red: 'bg-redAlpha-700/[0.71]',
+          blue: 'bg-blueAlpha-600/[0.62]',
+          green: 'bg-greenAlpha-600/[0.62]'
+        }
+      }
+    },
+    dark: {
+      bg:{
+        red: 'bg-red-950',
+        blue: 'bg-blue-950',
+        green: 'bg-green-950'
+      },
+      text: {
+        red: 'text-red-50',
+        blue: 'text-blue-50',
+        green: 'text-green-50'
+      },
+    }
+  }
